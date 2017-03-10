7cc028b81b98d5f5e0a960f686f33d038efa7a5c
zhijia,.zhang <jiahut@gmail.com>
2017-03-10 17:35:46 +0800

    update config

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import TodoApp from './todoapp.vue'
 import filters from './filters.js'
+
+Vue.use(Vuex)
+
 new Vue({
   el: '#todo',
   render: h => h(TodoApp)
